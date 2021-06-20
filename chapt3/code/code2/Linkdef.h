330b02307e1d261d39d4ffbbb38400b8c23f1de1
Test <zhli@pku.edu.cn>
2021-06-20 20:58:41 +0800

    Add files via upload

---
+++ chapt3/code/code2/Linkdef.h
@@ -0,0 +1,31 @@
+// Linkdef.h --- 
+// 
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+// 添加/修改以下内容
+#pragma link C++ class dssd+;
+#pragma link C++ class vector<dssd>+;
+#pragma link C++ class DSSD+;
+#pragma link C++ class vector<DSSD>+;
+
+#endif
+
+
+// 
+// Linkdef.h ends here
+
+
+
+
+
+
+
+
+
+
